3d712ac493284b006a411c99b142fbcf3ffc9800
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2023-01-09 17:41:43 +0100

    improving design

--- db/schema.rb
+++ db/schema.rb
@@ -51,12 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_191152) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "titles", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
