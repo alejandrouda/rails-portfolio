de4abd2b4b1e9044ca74a92fb085a5efdbdb9b4a
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2023-01-10 15:21:03 +0100

    Added tailwind css to project

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,32 @@
+// RESPONSIVE NAVBAR
+
+const navSlide = () => {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const nav = document.querySelector('.nav-responsive');
+  const navSecElements = document.querySelector('.nav-sec-elements');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burgerMenu.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5
+          }s`;
+      }
+    });
+    // if (navSecElements.style.animation) {
+    //   navSecElements.style.animation = '';
+    // } else {
+    //   navSecElements.style.animation = `navLinkFade 0.5s ease forwards 1s`;
+    // }
+
+    // Animate burger menu
+    burgerMenu.classList.toggle('toggle');
+  });
+};
+
+navSlide();
