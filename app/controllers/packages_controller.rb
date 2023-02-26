6986043d4f6e28cdade580b8e1d3c04eba1a6b45
alejandro udaquiola <alejandroudaquiola1@gmail.com>
2023-02-26 19:42:39 +0100

    Adding links and sections to website

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,2 +1,7 @@
 class PackagesController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index ]
+
+    def index
+        @packages = Package.all
+    end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,7 @@
 class ServicesController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :index ]
+
+    def index
+        @services = Service.all
+    end
 end
