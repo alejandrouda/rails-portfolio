10a3e2f803ba2730b81a029cd601da273f8f81cc
alejandro udaquiola <alejandroudaquiola1@gmail.com>
2023-03-31 18:16:43 +0200

    updating seeds

---
+++ db/schema.rb
@@ -0,0 +1,97 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_150332) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "article_category_tags", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "article_category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_category_id"], name: "index_article_category_tags_on_article_category_id"
+    t.index ["article_id"], name: "index_article_category_tags_on_article_id"
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contact_forms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.string "phone"
+    t.boolean "policy_consent"
+  end
+
+  create_table "packages", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "currency"
+    t.text "items", default: [], array: true
+  end
+
+  create_table "policies", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url_handle"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "image"
+    t.string "url"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "article_category_tags", "article_categories"
+  add_foreign_key "article_category_tags", "articles"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,10 @@ p "Creating packages..."
 
 web_package = Package.new(
     name:"Emprendimientos",
-    description: "a",
+    description: "Ideal para la presencia web de tu emprendimiento",
     price: 190,
     currency: "€",
-    items:["Diseño web responsive", "3 meses de mantenimiento"]
+    items:["Diseño web responsive", "Panel de admin", "3 meses de mantenimiento"]
 )
 
 web_package.save!
@@ -28,10 +28,10 @@ p web_package
 
 restaurant_package = Package.new(
     name:"Restaurant",
-    description: "a",
+    description: "La mejor opción para la presencia web de tu restaurante",
     price: 290,
     currency: "€",
-    items:["Diseño web responsive", "3 meses de mantenimiento", "QR Carta digital interactiva", "Sistema de delivery"]
+    items:["Diseño web responsive", "Panel de admin", "3 meses de mantenimiento", "QR Carta digital interactiva", "Sistema de delivery"]
 )
 
 restaurant_package.save!
@@ -39,25 +39,14 @@ p restaurant_package
 
 online_store_package = Package.new(
     name:"Tienda online",
-    description: "a",
+    description: "Utiliza una plataforma de e-commerce de última generación",
     price: 990,
     currency: "€",
-    items:["Diseño web responsive", "3 meses de mantenimiento", "Pagos seguros", "Búsqueda optimizada"]
+    items:["Diseño web responsive", "Panel de admin", "3 meses de mantenimiento", "Pagos seguros", "Búsqueda optimizada"]
 )
 
 online_store_package.save!
 p online_store_package
-
-custom_design_package = Package.new(
-    name:"Sitio personalizado",
-    description: "a",
-    price: 25,
-    currency: "€ / h",
-    items:["Diseño web a medida", "Funcionalidades personalizadas", "Integraciones"]
-)
-
-custom_design_package.save!
-p custom_design_package
 
 p"Packages created..."
 
